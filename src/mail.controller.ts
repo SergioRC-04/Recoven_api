e5e932eef7d6d6a6262d3d593319e3a856d7d9b8
SergioRC-04 <slcabana@uninorte.edu.co>
2026-06-03 07:33:55 -0500

    Correo funcional

--- src/mail.controller.ts
+++ src/mail.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './mail.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { CreateLeadDto } from './dto/create-lead.dto';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('mail')
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
 
-  @Get('mail')
+  @Get()
   getMail(): string {
-    return this.appService.getMail();
+    return this.mailService.getMail();
+  }
+
+  @Post('send-lead')
+  async sendLeadNotification(@Body() createLeadDto: CreateLeadDto) {
+    await this.mailService.sendLeadEmail(createLeadDto);
+    return { success: true, message: 'Solicitud enviada correctamente.' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { config } from 'dotenv';
+config();
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './mail.module';
+import { MailModule } from './mail.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MailModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
