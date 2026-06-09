5329ed91c5c0b536fe2a62e0373f30b3a798ccb3
SergioRC-04 <slcabana@uninorte.edu.co>
2026-06-09 17:23:42 -0500

    Update .gitignore and cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:4173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
