6ff0cd4e3bf0837f5f86bb703f974e5243a2cad3
SergioRC-04 <slcabana@uninorte.edu.co>
2026-06-12 15:20:02 -0500

    Cor disponible

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:4173',
-      'http://localhost:3000',
-    ],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
