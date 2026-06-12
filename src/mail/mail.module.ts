77e6c0b992b419cfd9ff839a62bf817a2fc79750
SergioRC-04 <slcabana@uninorte.edu.co>
2026-06-12 06:55:45 -0500

    Update mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -10,7 +10,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
       transport: {
         host: process.env.MAIL_HOST,
         port: parseInt(process.env.MAIL_PORT || '587'),
-        secure: process.env.MAIL_SECURE === 'true',
+        secure: process.env.MAIL_SECURE === 'false',
         auth: {
           user: process.env.MAIL_USER || '',
           pass: process.env.MAIL_PASSWORD || '',
