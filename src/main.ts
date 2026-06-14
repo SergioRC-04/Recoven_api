578be57a90aaa4a927962edf1a21ac925ddd5afd
SergioRC-04 <slcabana@uninorte.edu.co>
2026-06-14 14:01:56 -0500

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 1. Obtenemos el string de la variable de entorno
+  const corsEnv = process.env.CORS_ORIGINS;
+
+  // 2. Si existe, lo convertimos en Array con .split(','). Si no existe, usamos local por defecto.
+  const allowedOrigins = corsEnv ? corsEnv.split(',') : [];
+
+  // 3. Pasamos el array directamente a la configuración de CORS
   app.enableCors({
-    origin: true,
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
